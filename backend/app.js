9af8a7bfd0ae3418e8821bef7fa1d8eeeef91480
Bao Nguyen <baonq094@gmail.com>
2026-06-26 21:09:59 +0700

    install nodemon

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 
 app.use('/', (req, res) => {
-    res.send('Wellcome');
+    res.send('chào bạn ');
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
