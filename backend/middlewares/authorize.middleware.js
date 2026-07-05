d624d36625395babecb51cfc5ec1a58c24239d9f
Bao Nguyen <baonq094@gmail.com>
2026-07-05 07:15:03 +0700

    feat : implement authorize middleware

---
+++ backend/middlewares/authorize.middleware.js
@@ -0,0 +1,11 @@
+const authorize = (...roles) => {
+    return (req, res, next) => {
+        if (roles.includes(req.user.role)) {
+            return next();
+        }
+        else {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+    }
+}
+module.exports = authorize;
