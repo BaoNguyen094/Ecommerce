c9ed455192076fbf721cf532f8043b0e9c080b7a
Bao Nguyen <baonq094@gmail.com>
2026-07-13 06:15:56 +0700

    fix API createProduct

--- backend/services/product.service.js
+++ backend/services/product.service.js
@@ -2,84 +2,82 @@ const Product = require('../models/productModel');
 const Brand = require('../models/brandModel');
 const Category = require('../models/categoryModel');
 const mongoose = require('mongoose');
-const productVariant = require('../models/productVariantModel');
+const ProductVariant = require('../models/productVariantModel');
 const productService = {};
-const most =
-{
-    "name": "iPhone 16",
-    "brand": "6870.....",
-    "category": "6870.....",
-    "description": "...",
-    "images": [
-        "thumbnail.jpg"
-    ],
-    "variants": [
-        {
-            "sku": "IP16-BLK-128",
-            "price": 22000000,
-            "stock": 10,
-            "images": [
-                "black1.jpg",
-                "black2.jpg"
-            ],
-            "attribute": [
-                {
-                    "key": "color",
-                    "value": "Black"
-                },
-                {
-                    "key": "storage",
-                    "value": "128GB"
-                }
-            ]
-        }
-    ]
-}
+
 productService.createProduct = async (data) => {
     const { variants, ...product } = data;
     let category, brand;
     //validate:
-    if (!product.name) {
+    if (!product.name?.trim()) {
         throw new Error('Name required!');
     };
-    if (product.brand) {
-        brand = await Brand.findOne({ name: product.brand });
-        if (!brand) {
-            throw new Error('Brand not exists!');
+
+    if (!product.description?.trim()) {
+        throw new Error('Description is required!');
+    };
+    if (!Array.isArray(variants) || variants.length === 0) {
+        throw new Error('At least one variants is required!')
+    };
+    for (const variant of variants) {
+        const price = Number(variant.price);
+        if (Number.isNaN(price) || price <= 0) {
+            throw new Error('Price must be greater than 0.');
+        };
+        const stock = Number(variant.stock)
+        if (Number.isNaN(stock) || stock < 0) {
+            throw new Error('Stock cannot be negative.')
+        };
+        if (!Array.isArray(variant.images) || variant.images.length === 0) {
+            throw new Error(`Images of variant ${variant.sku} is required!`)
+        };
+        for (const image of variant.images) {
+            if (!(typeof image === 'string') || image.trim() === '') {
+                throw new Error('Image is not URL!');
+            }
         }
     };
-    if (product.category) {
-        category = await Category.findOne({ name: product.category });
-        if (!category) {
-            throw new Error('Category not exists!')
-        }
-    }
-    //check sku variant:
     const skuCount = new Map();
     for (const variant of variants) {
+        if (!variant.sku?.trim()) {
+            throw new Error('Sku is required!');
+        }
         skuCount.set(variant.sku, (skuCount.get(variant.sku) || 0) + 1);
-    }
+    };
     const duplicate = [];
     for (const [sku, count] of skuCount) {
         if (count > 1) duplicate.push(sku);
     };
     if (duplicate.length > 0) {
-        throw new Error(`Sku duplicate ${duplicate}`);
+        throw new Error(`Sku duplicate ${duplicate.join(', ')}`);
     };
-    const dataSku = await productVariant.distinct('sku');
-    for (const [sku] of skuCount) {
-        if (dataSku.includes(sku)) {
-            throw new Error('sku is exists!');
-        }
-    };
-    for (const variant of variants) {
-        if (!variant.price || Number(variant.price) === 0) {
-            throw new Error('Price alway than 0');
-        };
-        if (!variant.stock || Number(variant.stock) === 0) {
-            throw new Error('Stock alway than 0')
-        };
+    const listSku = [...skuCount.keys()];
+    const dataSku = await ProductVariant.find({
+        sku: {
+            $in: listSku
+        },
+    });
+    if (dataSku.length > 0) {
+        const existed = dataSku.map(v => v.sku);
+        throw new Error(`Sku is exists ${existed.join(', ')}`);
     }
+
+    if (mongoose.Types.ObjectId.isValid(product.brand)) {
+        brand = await Brand.findById(product.brand);
+        if (!brand) {
+            throw new Error('Brand not exists!');
+        }
+    } else {
+        throw new Error('Bran is required!')
+    };
+    if (mongoose.Types.ObjectId.isValid(product.category)) {
+        category = await Category.findById(product.category);
+        if (!category) {
+            throw new Error('Category not exists!')
+        };
+    } else {
+        throw new Error('Category is required!')
+    };
 
 
     const session = await mongoose.startSession();
@@ -94,7 +92,7 @@ productService.createProduct = async (data) => {
         for (const variant of variants) {
             variant.product = saveProduct._id;
         };
-        const saveVariant = await productVariant.create(variants, { session });
+        const saveVariant = await ProductVariant.create(variants, { session });
 
         await session.commitTransaction();
         return [saveProduct, saveVariant];
