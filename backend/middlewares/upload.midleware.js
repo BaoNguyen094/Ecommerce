d993013398eac0496808b6c4ce46a8fcb1705c73
Bao Nguyen <baonq094@gmail.com>
2026-07-20 08:35:34 +0700

    add fileFilter and limits in multer

--- backend/middlewares/upload.midleware.js
+++ backend/middlewares/upload.midleware.js
@@ -1,6 +1,15 @@
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+const allowTypes = new Set([
+    'image/jpeg',
+    'image/png',
+    'image/webp',
+]);
+const allowExtensions = ['JPEG', 'PNG', 'WEBBP'];
+const limits = {
+    fileSize: 5 * 1024 * 1024
+};
 const storage = multer.diskStorage({
     //Quyết định nơi lưu file
     destination(req, file, cb) {
@@ -14,5 +23,20 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+    if (allowTypes.has(file.mimetype)) {
+        return cb(null, true);
+    } else {
+        return cb(new Error(`Only ${allowExtensions.join(',')} image are allowed!`));
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+        files: 30
+    }
+});
 module.exports = upload;
